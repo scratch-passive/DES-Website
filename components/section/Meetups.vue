b8ffdccd49dd1d39c2846ae0b43d6918beee01cd
scratch-passive <isond@mymacewan.ca>
2024-01-08 14:02:26 -0700

    Changing data science meetup image to ImageKit link, deleting .svg

--- components/section/Meetups.vue
+++ components/section/Meetups.vue
@@ -113,7 +113,8 @@ export default {
                 },
                 dataScience: {
                     name: "Edmonton Data Science Community",
-                    logoSvg: "/img/logos/meetups/datascience-logo.svg",
+                    logoImg:
+                        "/meetups/DataScience_Nvpcx6-aY?updatedAt=1698789157558",
                     linkTo: "https://www.meetup.com/edmonton-data-science-community/",
                     description: `The Edmonton Data Community aims to build a safe space
                         for folks in the data space to connect, share knowledge and grow
